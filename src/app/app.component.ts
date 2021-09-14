e3fd577f7e3fe225157a5a05a19832df9d7528e0
Lukáš Matuška <lukynmatuska@gmail.com>
2021-09-14 16:05:52 +0200

    Add notification on new customer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
 import { SocketService } from './socketio/socket.service';
+import { Customer } from "./types/customer";
 
 @Component({
   selector: "app-root",
@@ -10,7 +12,26 @@ export class AppComponent {
   title = "PohLes Admin";
 
   public constructor(
-    private readonly socketService: SocketService,
+    private readonly wss: SocketService,
+    private readonly toastr: ToastrService,
   ) {
+
+    this.wss.on(
+      "new-customer",
+      (customer: Customer) => {
+        this.toastr.info(
+          `<span class="tim-icons icon-badge" [data-notify]="icon"></span> <b>New customer!</b> ${customer.name.first} ${customer.name.last}`,
+          '',
+          {
+            disableTimeOut: true,
+            closeButton: true,
+            enableHtml: true,
+            toastClass: "alert alert-info alert-with-icon",
+            positionClass: 'toast-top-right',
+          }
+        );
+      }
+    );
+
   }
 }
