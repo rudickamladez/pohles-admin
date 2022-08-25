ed5bcb340fce6dbe60dc0971af7be0ebc4a3a9e1
Lukáš Matuška <lukynmatuska@gmail.com>
2022-08-25 10:51:09 +0200

    I don't know what's wrong bruh.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { LoginComponent } from './auth/login/login.component';
@@ -17,9 +18,9 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    AdminLayoutComponent, 
-    CustomersComponent, 
+    AppComponent,
+    AdminLayoutComponent,
+    CustomersComponent,
     NotFoundComponent,
     LoginComponent
   ],
@@ -31,7 +32,8 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     ComponentsModule,
     NgbModule,
     ToastrModule.forRoot(),
-    OAuthModule.forRoot()
+    OAuthModule.forRoot(),
+    FontAwesomeModule,
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
--- src/app/components/customers/action-bar-customers/action-bar-customers.component.ts
+++ src/app/components/customers/action-bar-customers/action-bar-customers.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from 'src/app/services/customer.service';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-action-bar-customers',
   templateUrl: './action-bar-customers.component.html',
 })
 export class ActionBarCustomersComponent implements OnInit {
+  faCoffee = faCoffee;
 
   constructor(
     private readonly customerService: CustomerService,
