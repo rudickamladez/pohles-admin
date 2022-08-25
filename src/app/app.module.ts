8c43146b853563738f595735296417ed9d14199f
Lukáš Matuška <lukynmatuska@gmail.com>
2022-08-25 14:00:34 +0200

    Move HelloComponent to correct folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { ComponentsModule } from './components/components.module';
 import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
 import { CustomersComponent } from './pages/customers/customers.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { HelloComponent } from './hello/hello.component';
+import { HelloComponent } from './components/hello/hello.component';
 
 @NgModule({
   declarations: [
