8724b4e040b0ba121538df27524567a0a971f4b2
Lukáš Matuška <lukynmatuska@gmail.com>
2022-08-26 14:49:04 +0200

    Fix Cog button

--- src/app/layouts/admin/admin-layout.component.ts
+++ src/app/layouts/admin/admin-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-admin-layout",
@@ -7,6 +8,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AdminLayoutComponent implements OnInit {
   public sidebarColor: string = "green";
+  faCog = faCog;
 
   constructor() { }
 
