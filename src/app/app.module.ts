8877a6e07f4a652f30338b2d53b0e125fb28dd6c
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2022-09-12 19:54:32 +0200

    Add Tickets Scan page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { NotFoundComponent } from './pages/not-found/not-found.component'
 import { HelloComponent } from './components/hello/hello.component';
 import { TicketsComponent } from './components/tickets/tickets.component';
 import { TicketsActionBarComponent } from './components/tickets/tickets-action-bar/tickets-action-bar.component';
+import { TicketsScanComponent } from './components/tickets/tickets-scan/tickets-scan.component';
+import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
 
 @NgModule({
   declarations: [
@@ -29,6 +31,7 @@ import { TicketsActionBarComponent } from './components/tickets/tickets-action-b
     HelloComponent,
     TicketsComponent,
     TicketsActionBarComponent,
+    TicketsScanComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,7 +42,8 @@ import { TicketsActionBarComponent } from './components/tickets/tickets-action-b
     NgbModule,
     ToastrModule.forRoot(),
     OAuthModule.forRoot(),
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgxScannerQrcodeModule,
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
