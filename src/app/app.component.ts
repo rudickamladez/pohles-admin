cf825a40a5cbe5171f4d8bbb6ae9c90ae0f76df2
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2022-10-11 14:45:02 +0200

    Fix notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from './auth/auth.service';
 import { SocketService } from './socketio/socket.service';
-import { Customer } from './types/customer';
+import { Ticket } from './types/ticket';
 
 @Component({
   selector: 'app-root',
@@ -20,10 +20,31 @@ export class AppComponent {
     this.authService.init();
 
     this.wss.on(
-      "new-customer",
-      (customer: Customer) => {
+      "new-ticket",
+      (ticket: Ticket) => {
         this.toastr.info(
-          `<span class="tim-icons icon-badge" [data-notify]="icon"></span> <b>New customer!</b> ${customer.name.first} ${customer.name.last}`,
+          `<span class="tim-icons icon-money-coins" [data-notify]="icon"></span> <b>New ticket</b> at ${ticket.time.name}!`,
+          '',
+          {
+            disableTimeOut: true,
+            closeButton: true,
+            enableHtml: true,
+            toastClass: "alert alert-info alert-with-icon",
+            positionClass: 'toast-top-right',
+          }
+        );
+      }
+    );
+
+
+
+    this.wss.on(
+      "paid-ticket",
+      (ticket: Ticket) => {
+        this.toastr.info(
+          `<span class="tim-icons icon-coins" [data-notify]="icon"></span>
+          <b>Paid ticket</b> at ${ticket.time.name}!
+          by ${ticket.name.first} ${ticket.name.last}`,
           '',
           {
             disableTimeOut: true,
